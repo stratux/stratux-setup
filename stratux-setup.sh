d7a6fccceb9423a79e681c3135333822cee74400
Joseph Poirier <jdpoirier@gmail.com>
2016-03-15 22:17:57 -0500

    proper comparison

--- stratux-setup.sh
+++ stratux-setup.sh
@@ -49,7 +49,7 @@ apt-get install -y automake
 apt-get install -y hostapd
 
 REVISION="$(cat /proc/cpuinfo | grep Revision | cut -d ':' -f 2 | xargs)"
-if [ "$REVISION" -eq "a01041" ] || [ "$REVISION" -eq "a21041" ]; then
+if [ "$REVISION" == "a01041" ] || [ "$REVISION" == "a21041" ]; then
     # RPi2 specific hostapd binary
     echo "**** RPi2 specific hostapd installation *****"
     rm -f /usr/sbin/hostapd
@@ -77,7 +77,7 @@ if [ "$REVISION" -eq "a01041" ] || [ "$REVISION" -eq "a21041" ]; then
     if ! grep -q "options 8192cu rtw_power_mgnt=0 rtw_enusbss=0" "/etc/modprobe.d/8192cu.conf"; then
         echo "options 8192cu rtw_power_mgnt=0 rtw_enusbss=0" >>/etc/modprobe.d/8192cu.conf
     fi
-elif [ "$REVISION" -eq "a01041" ]; then
+elif [ "$REVISION" == "a01041" ]; then
     echo "**** RPi3 specific hostapd installation *****"
     cp -f ./files/hostapdRPi3.conf /etc/hostapd/hostapd.conf
 else
