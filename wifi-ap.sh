5b76498091d0e22e700efd6b96c0d1b68602e13f
jpoirier <jdpoirier@gmail.com>
2016-03-18 22:31:22 +0000

    Additional LSB parts

--- wifi-ap.sh
+++ wifi-ap.sh
@@ -158,6 +158,8 @@ cat <<EOT > /etc/init.d/wifiap
 
 ### BEGIN INIT INFO
 # Provides:          wifiap
+# Required-Start:
+# Required-Stop:
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
 # Short-Description: Stratux Wifi Access Point
