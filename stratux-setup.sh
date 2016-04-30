dfd625e56ebb90ea989d9947e4cabe1261bb671f
Joseph Poirier <jdpoirier@gmail.com>
2016-04-30 00:14:37 -0500

    use local hostapd_manager.sh if necessary

--- stratux-setup.sh
+++ stratux-setup.sh
@@ -465,6 +465,9 @@ echo "${YELLOW}**** Copying the hostapd_manager.sh utility... *****${WHITE}"
 if [ -f /root/stratux/image/hostapd_manager.sh ]; then
     chmod 755 /root/stratux/image/hostapd_manager.sh
     cp /root/stratux/image/hostapd_manager.sh /usr/bin/hostapd_manager.sh
+else
+    chmod 755 ${SCRIPTDIR}/hostapd_manager.sh
+    cp ${SCRIPTDIR}/hostapd_manager.sh /usr/bin/hostapd_manager.sh
 fi
 
 echo "${GREEN}...done${WHITE}"
